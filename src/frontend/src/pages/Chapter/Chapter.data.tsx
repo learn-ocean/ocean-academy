881f83bd6e8842174cddf336c124e0af5f78b3b3
Albert Peci <albert.peci@posteo.de>
2020-09-24 00:16:34 +0200

    add chapters

--- src/frontend/src/pages/Chapter/Chapter.data.tsx
+++ src/frontend/src/pages/Chapter/Chapter.data.tsx
@@ -1,21 +1,81 @@
 import { data as data1 } from '../Chapters/Chapter1'
 import { data as data2 } from '../Chapters/Chapter2'
 import { data as data3 } from '../Chapters/Chapter3'
+import { data as data4 } from '../Chapters/Chapter4'
+import { data as data5 } from '../Chapters/Chapter5'
+import { data as data6 } from '../Chapters/Chapter6'
+import { data as data7 } from '../Chapters/Chapter7'
+import { data as data8 } from '../Chapters/Chapter8'
+import { data as data9 } from '../Chapters/Chapter9'
+import { data as data10 } from '../Chapters/Chapter10'
+import { data as data11 } from '../Chapters/Chapter11'
+import { data as data12 } from '../Chapters/Chapter12'
+import { data as data13 } from '../Chapters/Chapter13'
 
 export const chapterData = [
   {
     pathname: '/chapter-1',
-    name: '1 - About Ocean Academy',
+    name: '1 - Why should you Care?',
     data: data1,
   },
   {
     pathname: '/chapter-2',
-    name: '2 - Why should you care?',
+    name: '2 - Key Concepts of Blockchain',
     data: data2,
   },
   {
     pathname: '/chapter-3',
-    name: '3 - Key Concepts of Blockchain',
+    name: '3 - Install Metamask',
     data: data3,
   },
+  {
+    pathname: '/chapter-4',
+    name: '4 - Get Play Money',
+    data: data4,
+  },
+  {
+    pathname: '/chapter-5',
+    name: '5 - Intro to Smart Contracts and Web3',
+    data: data5,
+  },
+  {
+    pathname: '/chapter-6',
+    name: '6 - Data and Web3',
+    data: data6,
+  },
+  {
+    pathname: '/chapter-7',
+    name: '7 -  New Data Workflows for Enterprises',
+    data: data7,
+  },
+  {
+    pathname: '/chapter-8',
+    name: '8 - Smart Contracts and an open source cloud computer',
+    data: data8,
+  },
+  {
+    pathname: '/chapter-9',
+    name: '9 - Smart Contracts: Hello world',
+    data: data9,
+  },
+  {
+    pathname: '/chapter-10',
+    name: '10 - Exploring the Ocean Token Contract',
+    data: data10,
+  },
+  {
+    pathname: '/chapter-11',
+    name: '11 - Smart Contract for Storing and Reading Data',
+    data: data11,
+  },
+  {
+    pathname: '/chapter-12',
+    name: '12 - Buying and Selling Data using Ocean Protocol',
+    data: data12,
+  },
+  {
+    pathname: '/chapter-13',
+    name: '13 - What is Ocean Protocol?',
+    data: data13,
+  }
 ]
