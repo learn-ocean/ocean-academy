6911cc1cbf40a303f5db1f31356c484ec94a2ad3
M3M3nt0L <14884414+MeMent0L@users.noreply.github.com>
2020-09-25 11:00:22 +0200

    Update Home.view.tsx

--- src/frontend/src/pages/Home/Home.view.tsx
+++ src/frontend/src/pages/Home/Home.view.tsx
@@ -13,9 +13,9 @@ export const HomeView = () => {
         <HomeContainer>
           <h1>Learn How to Build Web3 Apps for the Data Economy</h1>
           <p> Ocean 101 Academy provides a simple and practical introduction to anyone interested in monetizing data or analytic services using blockchain technology, starting from zero.
-          <br><br>You’ll be set-up with your own Ethereum digital wallet in less than 10 minutes.<br><br>
-          Through an interactive experience, you will get familiar with the main concepts of Web3, and gain deep understanding of Ocean Protocol; its inner workings and what it can do for you. 
-            <br>At the end of the 24 modules, participants are handed a completion certificate which grants access to the Ocean Ambassador community.
+          <span>You’ll be set-up with your own Ethereum digital wallet in less than 10 minutes.</span>
+          <span>Through an interactive experience, you will get familiar with the main concepts of Web3, and gain deep understanding of Ocean Protocol; its inner workings and what it can do for you.</span> 
+            <span>At the end of the 24 modules, participants are handed a completion certificate which grants access to the Ocean Ambassador community.</span>
           </p>
           <Link to="/chapter-1">
             <Button text="GET STARTED" color="primary" />
