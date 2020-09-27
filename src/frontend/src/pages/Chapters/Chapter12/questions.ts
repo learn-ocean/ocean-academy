7b012cda081debc4c997496f7635c210b1fba7f6
M3M3nt0L <14884414+MeMent0L@users.noreply.github.com>
2020-09-27 17:16:12 +0200

    Update questions.ts

--- src/frontend/src/pages/Chapters/Chapter12/questions.ts
+++ src/frontend/src/pages/Chapters/Chapter12/questions.ts
@@ -1,6 +1,6 @@
 export const questions = [
     {
-        question: "What is the advantage of the ocean protocols?",
+        question: "What is the main advantage of Ocean Protocol?",
         answers: [
             "It provides a standard way to access, buy and sell data",
             "It is a central database that collects all data sets in the world",
