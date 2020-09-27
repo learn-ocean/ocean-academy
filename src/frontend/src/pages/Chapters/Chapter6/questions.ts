4e488e62124227745d7e5c6d3700385e65161501
M3M3nt0L <14884414+MeMent0L@users.noreply.github.com>
2020-09-27 17:00:07 +0200

    Update questions.ts

--- src/frontend/src/pages/Chapters/Chapter6/questions.ts
+++ src/frontend/src/pages/Chapters/Chapter6/questions.ts
@@ -2,7 +2,7 @@ export const questions = [
     {
         question: "How can individuals benefit from a Web3 enabled data economy?",
         answers: [
-            "Full data ownershipData is only accessed but not controlled by service providers",
+            "Data is only accessed but not controlled by service providers",
             "(User) data can be used across multiple apps",
             "The user can use services without sharing any data"
         ],
