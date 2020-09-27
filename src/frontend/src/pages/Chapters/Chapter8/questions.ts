a5884d71bae1324de5d19924a1d9cd9344e23fdc
M3M3nt0L <14884414+MeMent0L@users.noreply.github.com>
2020-09-27 17:04:25 +0200

    Update questions.ts

--- src/frontend/src/pages/Chapters/Chapter8/questions.ts
+++ src/frontend/src/pages/Chapters/Chapter8/questions.ts
@@ -1,9 +1,9 @@
 export const questions = [
     {
-        question: "What is the main characteristic of the Ethereum blockchain?",
+        question: "Which one of these statements best describes the Ethereum blockchain?",
         answers: [
             "A decentralized computer without a central owner",
-            "An open cloud computer that is  operated by a company",
+            "An open cloud computer that is operated by a company",
             "A client to connect to a remote computer"
         ],
         response: "A decentralized computer without a central owner",
