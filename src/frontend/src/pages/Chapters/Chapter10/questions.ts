790f7ecaaf399fb6ee3a8b8d410ca56f3da5ae08
M3M3nt0L <14884414+MeMent0L@users.noreply.github.com>
2020-09-27 17:11:17 +0200

    Update questions.ts

--- src/frontend/src/pages/Chapters/Chapter10/questions.ts
+++ src/frontend/src/pages/Chapters/Chapter10/questions.ts
@@ -2,9 +2,10 @@ export const questions = [
     {
         question: "Open etherescan.io, search for the OCEAN token and see how many accounts are holding the OCEAN token currently.",
         answers: [
+            ">2'000 accounts",
+            ">6'000 accounts",
             ">12'000 accounts",
-            "<5'000 accounts",
-            ">150'000 accounts",
+            ">36'000 accounts",
         ],
         response: ">12'000 accounts",
         proposedResponse: ""
