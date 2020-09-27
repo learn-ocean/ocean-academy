1b3243027eb99513fcf8b650b88923ce23912916
M3M3nt0L <14884414+MeMent0L@users.noreply.github.com>
2020-09-27 17:19:54 +0200

    Update questions.ts

--- src/frontend/src/pages/Chapters/Chapter11/questions.ts
+++ src/frontend/src/pages/Chapters/Chapter11/questions.ts
@@ -2,7 +2,7 @@ export const questions = [
     {
         question: "Why is it impractical to store and share data directly on the Ethereum blockchain?",
         answers: [
-            "Smart contracts do not support data storage",
+            "Smart contracts do not support storage of large datasets",
             "Everything that interacts with a blockchain becomes pseudo-anonymous, and is therefore not private",
             "Usage costs",
             "All of these: data storage, privacy, and cost",
