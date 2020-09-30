7aab7b3dfc35d2fb5877310c5619b207386af492
M3M3nt0L <14884414+MeMent0L@users.noreply.github.com>
2020-09-30 17:29:31 +0200

    Update questions.ts

--- src/frontend/src/pages/Chapters/Chapter17/questions.ts
+++ src/frontend/src/pages/Chapters/Chapter17/questions.ts
@@ -2,12 +2,12 @@ export const questions = [
   {
     question: 'What is the difference between the Operator Service and the Operator Engine?',
     answers: [
-      'Operator Service instruct the Operator engine on what to compute',
+      'The Operator Service instructs the Operator Engine on what to compute',
       'Anyone with a ML pipeline, workflow or other script can make it available through the Operator Engine',
-      'Everyone can offer their computation power to become a Operator Service but only data to be a Operatoro Engine ',
-      'Operator engines based on the Operator Service workflow complete the data execution in the cluster.',
+      'Everyone can offer their computation power to become an Operator Service, but only data can be an Operator Engine',
+      'Operator Engines complete the data execution in the cluster based on the Operator Service workflow',
     ],
-    response: 'Operator engines based on the Operator Service workflow complete the data execution in the cluster.',
+    response: 'Operator Engines complete the data execution in the cluster based on the Operator Service workflow',
     proposedResponse: '',
   },
 ]
