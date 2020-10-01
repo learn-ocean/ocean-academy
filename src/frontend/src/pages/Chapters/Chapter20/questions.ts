ab6aa59d37a25fde8477d30bd3eb3d57cc21df9d
GL <gherardolattanzi@guerreton-13643.station>
2020-10-01 15:05:52 +0200

    Update questions.ts
    updated chap 20 quiz

--- src/frontend/src/pages/Chapters/Chapter20/questions.ts
+++ src/frontend/src/pages/Chapters/Chapter20/questions.ts
@@ -1,8 +1,8 @@
 export const questions = [
   {
-    question: 'WIP',
-    answers: ['option1', 'option2'],
-    response: '',
+    question: 'How can a Data Scientist exploit optimally Compute-to-Data feature?',
+    answers: ['By selling and consuming private data', 'By obtaining model predictions trained on private data', 'By modelling an algorithm with some training set only C2D can see', 'By monetizing pre developed algorithms in the Ocean Market'],
+    response: 'By obtaining model predictions trained on private data', By modelling an algorithm with some training set only C2D can see,
     proposedResponse: '',
   },
 ]
