92e04cecdcb5042d815fc5df3eb8bb825b65eb23
Albert Peci <albert.peci@posteo.de>
2020-10-01 20:27:29 +0200

    fix syntax

--- src/frontend/src/pages/Chapters/Chapter20/questions.ts
+++ src/frontend/src/pages/Chapters/Chapter20/questions.ts
@@ -1,8 +1,12 @@
 export const questions = [
   {
     question: 'How can a Data Scientist exploit optimally Compute-to-Data feature?',
-    answers: ['By selling and consuming private data', 'By obtaining model predictions trained on private data', 'By modelling an algorithm with some training set only C2D can see', 'By monetizing pre developed algorithms in the Ocean Market'],
-    response: 'By obtaining model predictions trained on private data', By modelling an algorithm with some training set only C2D can see,
+    answers: [
+    	'By selling and consuming private data', 
+    	'By obtaining model predictions trained on private data', 
+    	'By modelling an algorithm with some training set only C2D can see', 
+    	'By monetizing pre developed algorithms in the Ocean Market'],
+    response: 'By obtaining model predictions trained on private data',
     proposedResponse: '',
   },
 ]
