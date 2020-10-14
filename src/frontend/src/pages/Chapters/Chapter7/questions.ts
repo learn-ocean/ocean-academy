ec59f03a9ac613cddf8fe94bc35ec3b5adc543bf
M3M3nt0L <14884414+MeMent0L@users.noreply.github.com>
2020-10-14 08:50:58 +0200

    Update questions.ts

--- src/frontend/src/pages/Chapters/Chapter7/questions.ts
+++ src/frontend/src/pages/Chapters/Chapter7/questions.ts
@@ -5,13 +5,12 @@ export const questions = [
       'Data owners can get money for their data without sharing any of it with third parties',
       'Model builders and analysts can create new or better data-models that were not possible before because the data was just not accessible',
       'Model builders and analysts can reward data providers directly for their data, and only based on specific conditions (based on smart contracts)',
-      "Well, it's very hip to be on the blockchain right now",
+      "Well, it's very hip to be on the blockchain right now"
     ],
     responses: [
       'Data owners can get money for their data without sharing any of it with third parties',
       'Model builders and analysts can create new or better data-models that were not possible before because the data was just not accessible',
       'Model builders and analysts can reward data providers directly for their data, and only based on specific conditions (based on smart contracts)',
-      "Well, it's very hip to be on the blockchain right now",
     ],
   },
 ]
