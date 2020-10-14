86df2d69df6aaec51afd65cbc8ea7b0c3e0024c3
Albert Peci <albert.peci@posteo.de>
2020-10-14 08:17:49 +0200

    move about us to header

--- src/frontend/src/app/App.components/Header/Header.view.tsx
+++ src/frontend/src/app/App.components/Header/Header.view.tsx
@@ -27,6 +27,9 @@ export const HeaderView = ({ user, removeAuthUserCallback }: HeaderViewProps) =>
 function loggedOutHeader() {
   return (
     <HeaderLoggedOut>
+      <Link to="/about">
+        <HeaderMenuItem>ABOUT US</HeaderMenuItem>
+      </Link>
       <Link to="/sign-up">
         <HeaderMenuItem>SIGN UP</HeaderMenuItem>
       </Link>
@@ -40,6 +43,9 @@ function loggedOutHeader() {
 function loggedInHeader({ user, removeAuthUserCallback }: HeaderViewProps) {
   return (
     <HeaderLoggedIn>
+      <Link to="/about">
+        <HeaderMenuItem>ABOUT US</HeaderMenuItem>
+      </Link>
       <Link to={`/user/${user?.username}`}>
         <HeaderMenuItem>{user?.username}</HeaderMenuItem>
       </Link>
