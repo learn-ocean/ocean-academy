85e3a05011b2fdec6bb9beafa6a4e45d9c47c9fe
M3M3nt0L <14884414+MeMent0L@users.noreply.github.com>
2020-10-15 16:13:11 +0200

    Create question.ts
    Feel free to reword/simplify as you see fit

---
+++ src/frontend/src/pages/Chapters/Chapter2/question.ts
@@ -0,0 +1,12 @@
+export const questions = [
+    {
+        question: "What specificities make a public blockchain different from a public (editable) Google sheet?",
+        answers: [
+            "Anyone can make modifications to it",
+            "Anyone can see what is written on it",
+            "It can only accept pre-authorized types of modifications",
+            "It is not possible to erase content already written on it",
+        ],
+        responses: ["It can only accept pre-authorized types of modifications","It is not possible to erase content already written on it"],
+    }
+]
