44366cfebdb286639c49ca41f158fd7ff378c998
Sarah Kay <51705696+SarahKay99@users.noreply.github.com>
2020-11-10 21:50:27 +1100

    Update Hamburger.style.tsx

--- src/frontend/src/app/App.components/Hamburger/Hamburger.style.tsx
+++ src/frontend/src/app/App.components/Hamburger/Hamburger.style.tsx
@@ -1,83 +1,82 @@
 import styled from 'styled-components/macro'
+import { bgTextColor, primaryColor, backgroundColorLight, textColor } from 'styles'
 
-
-// prettier-ignore
-import { HamburgerBottomBackward, HamburgerBottomForward, HamburgerTopBackward, HamburgerTopForward, textColor } from '../../../styles'
-
-export const HamburgerStyledLeft = styled.div`
-  position: fixed;
-  left: 18px;
-  top: 18px;
-  overflow: visible;
-  margin: 0;
-  height: 14px;
-  box-sizing: content-box;
-  cursor: pointer;
-  z-index: 11;
+export const HeaderStyled = styled.div`
+  margin-bottom: 20px;
+  position: relative;
+  text-align: center;
+  height: 50px;
+  z-index: 1;
+  background-color: ${backgroundColorLight};
 `
 
-export const HamburgerStyledRight = styled.div`
-  position: fixed;
-  right: 18px;
-  top: 18px;
-  overflow: visible;
-  margin: 0;
-  height: 14px;
-  box-sizing: content-box;
-  cursor: pointer;
-  z-index: 11;
-  display: none;
+export const HeaderLogo = styled.img`
+  padding: 11px;
+  z-index: 1;
+  margin: auto;
+`
+
+export const HeaderLoggedOut = styled.div`
+  position: absolute;
+  top: 0;
+  right: 10px;
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+  grid-gap: 10px;
 
   @media (max-width: 1130px) {
-    display: flex;
+    display: none;
   }
 `
 
-export const HamburgerBox = styled.div`
-  position: relative;
-  display: inline-block;
-  width: 24px;
-  height: 14px;
-`
-
-export const HamburgerInner = styled.div`
+export const HeaderLoggedIn = styled.div`
   position: absolute;
-  width: 24px;
-  height: 1px;
-  border-radius: 1px;
-  will-change: transform;
-  background-color: ${textColor};
-`
-
-export const HamburgerInnerTop = styled(HamburgerInner)`
   top: 0;
+  right: 0;
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+  grid-gap: 10px;
+  text-transform: uppercase;
 
-  &.true {
-    animation: ${HamburgerTopForward} 1s linear;
-    animation-fill-mode: forwards;
-  }
-
-  &.false {
-    animation: ${HamburgerTopBackward} 1s linear;
-    animation-fill-mode: forwards;
+  @media (max-width: 1130px) {
+    display: none;
   }
 `
 
-export const HamburgerInnerMiddle = styled(HamburgerInner)`
-  display: block;
-  top: calc(50% - 1px);
+export const HeaderMenuItem = styled.div`
+  position: relative;
+  color: ${textColor};
+  line-height: 50px;
+  font-size: 14px;
+  font-weight: 700;
+  display: inline-block;
+  padding: 0 20px;
+
+  &.login {
+    background-color: ${primaryColor};
+    color: ${bgTextColor};
+    width: 128px;
+    display: grid;
+    grid-template-columns: auto 50px;
+    text-align: right;
+
+    > div {
+      line-height: 50px;
+    }
+
+    > svg {
+      height: 28px;
+      width: 28px;
+      margin: 11px;
+      stroke: ${bgTextColor};
+    }
+  }
+
+  @media (max-width: 1440px) {
+    padding: 0 10px;
+  }
 `
 
-export const HamburgerInnerBottom = styled(HamburgerInner)`
-  bottom: 1px;
-
-  &.true {
-    animation: ${HamburgerBottomForward} 1s linear;
-    animation-fill-mode: forwards;
-  }
-
-  &.false {
-    animation: ${HamburgerBottomBackward} 1s linear;
-    animation-fill-mode: forwards;
-  }
+export const FillFlex = styled.div`
+  flex: 1;
 `
