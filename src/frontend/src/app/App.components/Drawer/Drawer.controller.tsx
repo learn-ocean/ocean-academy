5ee771122d8f17e21a5efbdfcaa48f050c4dd317
Sarah Kay <51705696+SarahKay99@users.noreply.github.com>
2020-11-10 21:46:17 +1100

    Update Drawer.controller.tsx

--- src/frontend/src/app/App.components/Drawer/Drawer.controller.tsx
+++ src/frontend/src/app/App.components/Drawer/Drawer.controller.tsx
@@ -4,9 +4,10 @@ import { useHistory, useLocation } from 'react-router-dom'
 import { State } from 'reducers'
 
 import { hideDrawer } from './Drawer.actions'
-import { DrawerView } from './Drawer.view'
+import { ChapterDrawerView, LoginDrawerView } from './Drawer.view'
 
-export const Drawer = () => {
+// CHANGE DRAWER ==> CHAPTERDRAWER
+export const ChapterDrawer = () => {
   const dispatch = useDispatch()
   const showing = useSelector((state: State) => state.drawer && state.drawer.showing)
   const user = useSelector((state: State) => state && state.auth && state.auth.user)
@@ -16,15 +17,40 @@ export const Drawer = () => {
     dispatch(hideDrawer())
   }
 
-  function removeAuthUserCallback() {}
+  function removeAuthUserCallback() { }
 
   return (
-    <DrawerView
+    <ChapterDrawerView
       showing={showing}
       hideCallback={hideCallback}
       pathname={pathname}
-      user={user}
-      removeAuthUserCallback={removeAuthUserCallback}
+      user_drawer={user}
+      removeAuthUserCallback_drawer={removeAuthUserCallback}
+    />
+  )
+}
+
+export const LoginDrawer = () => {
+  const dispatch = useDispatch()
+  const showing = useSelector((state: State) => state.drawer && state.drawer.showing)
+  const user = useSelector((state: State) => state && state.auth && state.auth.user)
+  const { pathname } = useLocation()
+
+  const hideCallback = () => {
+    dispatch(hideDrawer())
+  }
+
+  function removeAuthUserCallback() { }
+
+  return (
+    <LoginDrawerView
+      user_header={user}
+      removeAuthUserCallback_header={removeAuthUserCallback}
+      showing={showing}
+      hideCallback={hideCallback}
+      pathname={pathname}
+      user_drawer={user}
+      removeAuthUserCallback_drawer={removeAuthUserCallback}
     />
   )
 }
