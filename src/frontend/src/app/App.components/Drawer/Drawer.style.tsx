65010a1d664a95f3bbe4b2122cac177c524b6848
Sarah Kay <51705696+SarahKay99@users.noreply.github.com>
2020-11-10 21:45:42 +1100

    Update Drawer.style.tsx

--- src/frontend/src/app/App.components/Drawer/Drawer.style.tsx
+++ src/frontend/src/app/App.components/Drawer/Drawer.style.tsx
@@ -85,3 +85,38 @@ export const DrawerItem = styled.div`
     stroke: ${primaryColor};
   }
 `
+
+// remove display: none;
+export const DrawerStyledLogin = styled.div`
+  display: none;
+  position: fixed;
+  top: 0;
+  right: -1px;
+  height: 100vh;
+  z-index: 10;
+  width: 300px;
+  max-width: calc(100vw - 20px);
+  padding: 40px 20px 20px 30px;
+  background-color: ${backgroundColorLight};
+  box-shadow: 1px 7px 14px -5px rgba(0, 0, 0, 0.2);
+  transform: translate3d(0px, 0, 0);
+  transition: 0.2s ease-in-out;
+  will-change: transform;
+  overflow: scroll;
+
+  h1 {
+    color: ${textColor};
+  }
+
+  &.true {
+    transform: translate3d(-200px, 0, 0);
+  }
+
+  &.false {
+    transform: translate3d(-200px, 0, 0);
+  }
+
+  a {
+    color: ${textColor};
+  }
+`
