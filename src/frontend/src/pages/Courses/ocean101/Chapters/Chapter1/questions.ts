7d9d2477520359d988d738b98185b5f492f4cda6
GL <gherardolattanzi@guerreton-13643.station>
2021-01-18 14:17:14 +0100

    updated question 1, 10 and 24

--- src/frontend/src/pages/Courses/ocean101/Chapters/Chapter1/questions.ts
+++ src/frontend/src/pages/Courses/ocean101/Chapters/Chapter1/questions.ts
@@ -2,7 +2,7 @@ export const questions = [
     {
         question: "What best describes how you, personally, could benefit from bridging data supply and demand with Ocean Protocol?",
         answers: [
-            "I have (inactive) data, which I could monetize with Ocean Protocol",
+            "I have (inactive) data, which I could monetize via the Ocean Protocol stack",
             "I have some expertise in data science and/or AI, which i could monetize with Ocean Protocol",
             "I have both inactive data, and some expertise, and I could monetize both with Ocean Protocol",
             "I can’t monetize data or expertise right now, but I will benefit from new data economy built on Ocean Protocol indirectly"
--- src/frontend/src/pages/Courses/ocean101/Chapters/Chapter10/questions.ts
+++ src/frontend/src/pages/Courses/ocean101/Chapters/Chapter10/questions.ts
@@ -2,11 +2,11 @@ export const questions = [
     {
         question: "Open etherescan.io, search for the OCEAN token and see how many accounts are holding the OCEAN token currently.",
         answers: [
-            ">2'000 accounts",
-            ">6'000 accounts",
+            "<2'000 accounts",
+            "<6'000 accounts",
             ">12'000 accounts",
-            ">36'000 accounts",
+            ">20'000 accounts",
         ],
-        responses: [">12'000 accounts"],
+        responses: [">12'000 accounts", ">20'000 accounts"],
     }
 ]
