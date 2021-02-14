e795207e73a4f97f9713ef77f01dab571ae4f6dd
oceanByte <albert.peci@posteo.de>
2021-02-14 20:13:31 +0100

    feat (certificate): add gas price to truffle config

--- src/contracts/truffle-config.js
+++ src/contracts/truffle-config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const Web3 = require('web3');
 const web3 = new Web3();
-console.log(web3.utils.toWei('250', 'gwei'));
+const gasPrice = web3.utils.toWei('50', 'gwei');
 
 module.exports = {
   contracts_build_directory: '../frontend/src/abis',
@@ -18,24 +18,27 @@ module.exports = {
       host: '127.0.0.1',
       port: 7545,
       network_id: '*',
+      gasPrice
     },
     test: {
       host: '127.0.0.1',
       port: 7545,
       network_id: '*',
+      gasPrice
     },
     rinkeby: {
       provider: function () {
         return new HDWalletProvider(process.env.MNEMONIC, `https://rinkeby.infura.io/v3/${process.env.INFURA_ID}`)
       },
       network_id: 4,
+      gasPrice
     },
     mainnet: {
       provider: function () {
         return new HDWalletProvider(process.env.MNEMONIC, `https://mainnet.infura.io/v3/${process.env.INFURA_ID}`)
       },
       network_id: 1,
-      gasPrice: web3.utils.toWei('250', 'gwei'),
+      gasPrice
     },
   },
 }
