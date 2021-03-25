8f3e66af6e0ab54426b8f215d4d2f9c779b12f47
oceanByte <albert.peci@posteo.de>
2021-03-25 18:36:46 +0100

    refactor(home): update header section text

--- src/frontend/src/pages/Home/Home.view.tsx
+++ src/frontend/src/pages/Home/Home.view.tsx
@@ -9,54 +9,54 @@ export const HomeView = () => {
   return (
     <HomeStyled>
       <img className={"mantaray"} alt="mantaray-animated" src="/mantaray-full.svg" />
-        <HomeContainer>
-          {/*Removed "and analytic services", analytics is also data. providing clear and concise header */}
-          <h1>Learn to monetize data using blockchain technology</h1>
-          <p>
-          Ocean Academy 101 is a community initiative providing a simple and practical introduction to Ocean Protocol starting from zero. For free. Anyone who successfully completes Ocean 101 can go on to become and Ocean Ambassador. 
+      <HomeContainer>
+        {/*Removed "and analytic services", analytics is also data. providing clear and concise header */}
+        <h1>Learn to monetize data using blockchain technology</h1>
+        <p>
+          Ocean Academy 101 is a community initiative providing a simple and practical introduction to Ocean Protocol starting from zero. For free. Anyone who completes Ocean 101 can go on to become an Ocean Ambassador.
           </p>
-          <div className={"communityButton"}>
-            <Link to="/ocean101/chapter-1">
-              <Button text="GET STARTED" color="primary" />
+        <div className={"communityButton"}>
+          <Link to="/ocean101/chapter-1">
+            <Button text="GET STARTED" color="primary" />
+          </Link>
+          <a target="_blank" rel="noopener noreferrer" href="https://discord.gg/P264XprdJy">
+            <Button text="JOIN THE COMMUNITY" color="primary" />
+          </a>
+        </div>
+      </HomeContainer>
+
+      <HomeCourse className={"about"}>
+        <p>Through a free, interactive experience, you will learn to use Ocean Protocol to create value from data science in the Web3 space.</p>
+        <p><b>Ocean101</b> provides a 23-module introduction and gives an official completion certificate writable on the blockchain (NFT).</p>
+        <p><b>Data DeFi</b> is a 6-module overview of Decentralized Finance with tokenized data assets.</p>
+      </HomeCourse>
+
+      <HomeStyled className={"modules"}>
+        <HomeCourseGridWrapper>
+          <h1>Available Modules</h1>
+          <p>Get started on the module you are interested in.</p>
+          <HomeCourseGrid>
+            <Link to={'ocean101/chapter-1'}>
+              <CourseBox
+                title={"Ocean 101"}
+                shortDescription={"Learn the fundamentals of Ocean, get your ETH wallet in 10 minutes!"}
+                noChapters={23}
+                completed={false}
+              />
             </Link>
-            <a target="_blank" rel="noopener noreferrer" href="https://discord.gg/P264XprdJy">
-              <Button text="JOIN THE COMMUNITY" color="primary" />
-            </a>
-          </div>
-        </HomeContainer>
 
-        <HomeCourse className={"about"}>
-          <p>Through a free, interactive experience, you will learn to use Ocean Protocol to create value from data science in the Web3 space.</p>
-          <p><b>Ocean101</b> provides a 23-module introduction and gives an official completion certificate writable on the blockchain (NFT).</p>
-          <p><b>Data DeFi</b> is a 6-module overview of Decentralized Finance with tokenized data assets.</p>
-        </HomeCourse>
+            <Link to={'introToDataDefi/chapter-1'} >
+              <CourseBox
+                title={"Intro To Data DeFi"}
+                shortDescription={"Introduction to Decentralized Finance with Data"}
+                noChapters={6}
+                completed={false}
+              />
+            </Link>
 
-        <HomeStyled className={"modules"}>
-          <HomeCourseGridWrapper>
-            <h1>Available Modules</h1>
-            <p>Get started on the module you are interested in.</p>
-            <HomeCourseGrid>
-              <Link to={'ocean101/chapter-1'}>
-                <CourseBox
-                  title={"Ocean 101"}
-                  shortDescription={"Learn the fundamentals of Ocean, get your ETH wallet in 10 minutes!"}
-                  noChapters={23}
-                  completed={false}
-                />
-              </Link>
-
-              <Link to={'introToDataDefi/chapter-1'} >
-                <CourseBox
-                  title={"Intro To Data DeFi"}
-                  shortDescription={"Introduction to Decentralized Finance with Data"}
-                  noChapters={6}
-                  completed={false}
-                />
-              </Link>
-
-            </HomeCourseGrid>
-          </HomeCourseGridWrapper>
-        </HomeStyled>
+          </HomeCourseGrid>
+        </HomeCourseGridWrapper>
+      </HomeStyled>
     </HomeStyled>
   )
 }
