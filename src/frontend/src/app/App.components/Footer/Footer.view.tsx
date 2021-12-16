9a0548efdacb982d3aa3e558c9d6fba0afa4e658
mwmouawad <mwmouawad@gmail.com>
2021-12-16 17:27:44 +0100

    footer: removed commented section

--- src/frontend/src/app/App.components/Footer/Footer.view.tsx
+++ src/frontend/src/app/App.components/Footer/Footer.view.tsx
@@ -70,24 +70,3 @@ export const FooterView: any = () => (
 
   </FooterStyled>
 )
-
-
-// <div>
-// <h2>
-//   Follow us
-// </h2>
-// </div>
-// <br />
-// This site is protected by reCAPTCHA and the Google{' '}
-// <u>
-//   <a href="https://policies.google.com/privacy" target="_blank" rel="noopener noreferrer">
-//     Privacy Policy
-//   </a>
-// </u>{' '}
-// and{' '}
-// <u>
-//   <a href="https://policies.google.com/terms" target="_blank" rel="noopener noreferrer">
-//     Terms of Service
-//   </a>
-// </u>{' '}
-// apply.
