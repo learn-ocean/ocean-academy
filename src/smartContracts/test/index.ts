6f404b36368cdb1cb6e29c80b58daf7aa8cfd228
mwmouawad <mwmouawad@gmail.com>
2022-05-03 01:12:50 +0200

    hardhat unit testing

--- src/smartContracts/test/index.ts
+++ src/smartContracts/test/index.ts
@@ -1,19 +1,47 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
+import { Contract } from "ethers";
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("Referral", function () {
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  let Referral;
+  let OceanToken
+  let referral: Contract;
+  let oceanToken: Contract;
+  let owner: SignerWithAddress;
+  let addr1: SignerWithAddress;
+  let addr2: SignerWithAddress;
+  let addrs: SignerWithAddress[];
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+  beforeEach(async function () {
+    Referral = await ethers.getContractFactory("OceanAcademyReferral");
+    OceanToken = await ethers.getContractFactory("OceanToken");
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    oceanToken = await OceanToken.deploy(addr2.address);
+    referral = await Referral.deploy(owner.address, oceanToken.address);
+  });
+
+  // You can nest describe calls to create subsections.
+  describe("Deployment", function () {
+
+    it("Should set the right owner", async function () {
+      expect(await referral.owner()).to.equal(owner.address);
+    });
+
+    it("Check if owner is set.", async function () {
+      expect(await referral.admin()).to.equal(owner.address);
+    });
+
+    it("Check if ocean address is set.", async function () {
+      expect(await referral.oceanAddress()).to.equal(oceanToken.address);
+    });
+
+    it("Check that contract balance is 0.", async function () {
+      expect(await referral.getOceanBalance()).to.equal(0);
+    });
+
   });
 });
