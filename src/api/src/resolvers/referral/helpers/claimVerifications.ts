4e2d19e58d70613654f6161fa868f443fb04782b
mwmouawad <mwmouawad@gmail.com>
2022-06-15 13:09:36 -0400

    fixed brightId verification bug

--- src/api/src/resolvers/referral/helpers/claimVerifications.ts
+++ src/api/src/resolvers/referral/helpers/claimVerifications.ts
@@ -2,7 +2,7 @@ import {verifyContextId} from "brightid_sdk"
 import { ObjectId } from "mongodb"
 import { ReferralModel } from "../../../shared/referral/Referral"
 import { ResponseError } from "../../../shared/mongo/ResponseError"
-import {ethers} from 'ethers';
+import { ethers } from "ethers"
 
 export const OCEAN_DAO_CONTEXT = "OceanDAO"
 
@@ -10,16 +10,18 @@ export const verifyBrightId = async(userId: ObjectId, publicAddress:string)=>{
         const resp: any = await verifyContextId(OCEAN_DAO_CONTEXT, publicAddress);
         if(resp.data && resp.data.error)
             throw new ResponseError(400, "BrightID error: " + resp.data.errorMessage)
-
+        
         for(const addr of resp.contextIds){
+            let addrChecksum = ethers.utils.getAddress(addr);
             const referralsQuery = await ReferralModel.find(
                 {$and: [
-                {publicAddress: addr}, 
+                {publicAddress: addrChecksum}, 
                 {referrerId:{$ne:userId}}]
             })
             if(referralsQuery.length > 0)
                 throw new ResponseError(400, 'Not elligible to reward.')
         }
+        return;
 }
 
 export const verifyNonce = async(publicAddress: string, signedNonce: string, nonce: number) => {
