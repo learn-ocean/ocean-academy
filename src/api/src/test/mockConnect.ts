a5a50c43c6d1b10b3c1290f83553638060d71afe
mwmouawad <mwmouawad@gmail.com>
2022-06-16 13:08:52 -0400

    fixed mongoose warnings

--- src/api/src/test/mockConnect.ts
+++ src/api/src/test/mockConnect.ts
@@ -1,5 +1,9 @@
 import * as mongoose from 'mongoose'
 
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
 export const mockConnect = async (): Promise<void> => {
   await mongoose.connect(process.env.MONGO_URL as string, {
     useNewUrlParser: true,
